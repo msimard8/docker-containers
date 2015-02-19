2b9a586ee5f5de0ba4a759e2361f9444902bf5c3
Michael Simard <msimard8@gmail.com>
2015-02-19 12:33:46 -0800

    Remved mkdir from patch script, now done in docker

--- ddclient/patch.sh
+++ ddclient/patch.sh
@@ -2,11 +2,13 @@
 
 echo Patching 
 
-mkdir /root/ddclient-patch
+
 cp /usr/sbin/ddclient /root/ddclient-patch/
 cd /root/ddclient-patch/
 
 patch -b -p0 -i ddclient.patch
+
+
 
 cp /root/ddclient-patch/ddclient /usr/sbin/
 
